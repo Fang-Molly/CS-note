24b83059ca0236a4f2362b1f41e43bdd999c47f0
Fang <49796592+Fang-Molly@users.noreply.github.com>
2020-06-28 11:12:44 -0400

    Update chapter8_ex1.py

--- python/Exercises/chapter8_ex1.py
+++ python/Exercises/chapter8_ex1.py
@@ -1,5 +1,5 @@
-# Chapter 8  
-# Exercise 1: Write a function called chop that takes a list and modifies it, removing the first and last elements, and returns None. Then write a function called middle that takes a list and returns a new list that contains all but the first and last elements.
+# Chapter 8  Lists
+# Exercise 1: Write a function called chop that takes a list and modifies it, removing the first and last elements, and returns None. Then write a function called middle that takes a list and returns a new list that contains all but the first and last elements.  
 
 def chop(list):
     del list[0]

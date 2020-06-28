c34ed73f7f2fa2639a5247805517fc012115a318
Fang <49796592+Fang-Molly@users.noreply.github.com>
2020-06-28 12:22:51 -0400

    Update chapter8_ex1.py

--- python/Exercises/chapter8_ex1.py
+++ python/Exercises/chapter8_ex1.py
@@ -1,12 +1,17 @@
 """
-
-Exercise 1: Write a function called chop that takes a list and modifies it, removing the first and last elements, and returns None. Then write a function called middle that takes a list and returns a new list that contains all but the first and last elements.  
-
 Python for Everybody: Exploring Data Using Python 3
 by Charles R. Severance
+"""
+
+"""Chapter 8: Lists"""
+
+"""
+Exercise 1: 
+Write a function called chop that takes a list and modifies it, removing the first 
+and last elements, and returns None. Then write a function called middle that takes 
+a list and returns a new list that contains all but the first and last elements.  
 
 Solution by Fang, June 28, 2020
-
 """
 
 

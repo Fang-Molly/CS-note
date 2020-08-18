198ac7eb00b6d532f59a05fcb506184214d34ce5
Fang <49796592+Fang-Molly@users.noreply.github.com>
2020-08-18 15:45:15 -0400

    Update and rename exercise 0.py to Exercise 0 The Setup.py

---
+++ python/learn_python3_hard_way/Exercise 0 The Setup.py
@@ -0,0 +1,9 @@
+# Learn how to use Terminal on macOS
+
+# Download Pyhthon3 
+
+# Download the Atom text editor and install it
+
+# Run python3 in terminal program by typing the name and hitting Return
+
+# Type quit（）to get out of python3

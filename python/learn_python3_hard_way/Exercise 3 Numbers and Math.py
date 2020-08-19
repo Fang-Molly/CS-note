b415dcddf588f31e3d809560e72a5813f6105002
Fang <49796592+Fang-Molly@users.noreply.github.com>
2020-08-18 23:29:24 -0400

    Create Exercise 3 Numbers and Math.py

---
+++ python/learn_python3_hard_way/Exercise 3 Numbers and Math.py
@@ -0,0 +1,26 @@
+print("I wiil now count my chickens:")
+
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+
+print("Now I will count the eggs:")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+
+print(3 + 2 < 5 - 7)
+
+
+print("what is 3+2?", 3 + 2)
+print("what is 5-7?", 5 - 7)
+
+
+print("oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5<= -2)

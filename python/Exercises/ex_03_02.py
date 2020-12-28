fb1686f98b6ced4fa91807e426957710fa2b9728
Fang <49796592+Fang-Molly@users.noreply.github.com>
2020-12-28 00:05:35 -0500

    Update ex_03_02.py

--- python/Exercises/ex_03_02.py
+++ python/Exercises/ex_03_02.py
@@ -1,12 +1,13 @@
-# Exercise 2: Rewrite your pay program using try and except so that your program handles non-numeric input gracefully by printing a message and exiting the program. The following shows two executions of the program:
+"""
+Exercise 2: Rewrite your pay program using try and except so that your program handles non-numeric input gracefully by printing a message and exiting the program. The following shows two executions of the program:
 
-# Enter Hours: 20  
-# Enter Rate: nine  
-# Error, please enter numeric input  
+Enter Hours: 20  
+Enter Rate: nine  
+Error, please enter numeric input  
 
-# Enter Hours: forty  
-# Error, please enter numeric input
-
+Enter Hours: forty  
+Error, please enter numeric input
+"""
 
 hour = input('Enter Hours: ')
 

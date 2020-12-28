c9162d5cca27ed0edc5d2afceb6173432e746583
Fang <49796592+Fang-Molly@users.noreply.github.com>
2020-12-27 23:59:17 -0500

    Update ex_03_02.py

--- python/Exercises/ex_03_02.py
+++ python/Exercises/ex_03_02.py
@@ -9,6 +9,7 @@
 
 
 hour = input('Enter Hours: ')
+
 try:
     hour = float(hour)
     rate = input('Enter Rate: ')
@@ -21,4 +22,5 @@ if hour <= 40:
     pay = hour * rate
 else:
     pay = 40 * rate + (hour - 40) * (rate * 1.5)
+    
 print('Pay:',pay)

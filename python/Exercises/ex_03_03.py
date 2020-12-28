7d8da8d597904df7d4fb76d39599cfaba7590406
Fang <49796592+Fang-Molly@users.noreply.github.com>
2020-12-28 00:03:16 -0500

    Update ex_03_03.py

--- python/Exercises/ex_03_03.py
+++ python/Exercises/ex_03_03.py
@@ -1,3 +1,30 @@
+# Exercise 3: Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range, print an error message. If the score is between 0.0 and 1.0, print a grade using the following table:
+
+# Score    Grade  
+# >= 0.9     A   
+# >= 0.8     B   
+# >= 0.7     C   
+# >= 0.6     D  
+# <0.6       F  
+
+# Enter score: 0.95  
+# A
+
+# Enter score: perfect  
+# Bad score
+
+# Enter score: 10.0   
+# Bad score
+
+# Enter score: 0.75   
+# C
+
+# Enter score: 0.5  
+# F
+
+# Run the program repeatedly as shown above to test the various different values for input.
+
+
 score = input('Please enter a score between 0.0 and 1.0: ')
 
 try:

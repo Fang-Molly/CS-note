ec08167c14600510c9fe135ee9775eca06f4e724
Fang <49796592+Fang-Molly@users.noreply.github.com>
2020-12-28 00:04:54 -0500

    Update ex_03_01.py

--- python/Exercises/ex_03_01.py
+++ python/Exercises/ex_03_01.py
@@ -1,8 +1,10 @@
-# Exercise 1: Rewrite your pay computation to give the employee 1.5 times the hourly rate for hours worked above 40 hours.
-# Enter Hours: 45  
-# Enter Rate: 10  
-# Pay: 475.0
+"""
+Exercise 1: Rewrite your pay computation to give the employee 1.5 times the hourly rate for hours worked above 40 hours.
 
+Enter Hours: 45  
+Enter Rate: 10  
+Pay: 475.0
+"""
 
 hour = input('Enter Hours: ')
 hour = float(hour)

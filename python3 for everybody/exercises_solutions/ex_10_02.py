ed872f79a6e7af90196ed5c2844f719c7eb6fb21
Fang <49796592+Fang-Molly@users.noreply.github.com>
2021-02-23 21:33:00 -0500

    Update ex_10_02.py

--- python3 for everybody/exercises_solutions/ex_10_02.py
+++ python3 for everybody/exercises_solutions/ex_10_02.py
@@ -1,7 +1,7 @@
 '''
-Python For Everybody: Exploring Data in Python 3 (by Charles R. Severance)
+Python For Everybody: Exploring Data in Python 3 (by Charles R. Severance)  
 
-Exercise 10.2: This program counts the distribution of the hour of the day for each of the messages. You can pull the hour from the “From” line by finding the time string and then splitting that string into parts using the colon character. Once you have accumulated the counts for each hour, print out the counts, one per line, sorted by hour as shown below.
+Exercise 10.2: This program counts the distribution of the hour of the day for each of the messages. You can pull the hour from the “From” line by finding the time string and then splitting that string into parts using the colon character. Once you have accumulated the counts for each hour, print out the counts, one per line, sorted by hour as shown below.  
 
 python timeofday.py            
 Enter a file name: mbox-short.txt         

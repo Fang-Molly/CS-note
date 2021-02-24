1572be53d6e5a6964ed4d76c19512c9f225fbf8c
Fang <49796592+Fang-Molly@users.noreply.github.com>
2021-02-23 21:52:32 -0500

    Delete Learn Python3 the Hard Way directory

--- Learn Python3 the Hard Way/ex11.py
+++
@@ -1,8 +0,0 @@
-print ("How old are you?"),
-age = input()
-print ("How tall are you?"),
-height = input()
-print ("How much do you weight?"),
-weight = input()
-
-print ("So, you're %r old, %r tall and %r heavy." % (age, height, weight))
--- Learn Python3 the Hard Way/ex12.py
+++
@@ -1,1 +0,0 @@
-age = input("How old are you?")height = input("How tall are you?")weight = input("How much do you weight?")print ("So, you're %r old, %r tall and %r heavy." %(age, height, weight))
